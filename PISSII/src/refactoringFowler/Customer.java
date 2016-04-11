651afffff06a5fd5feeebd5df334451fa4dd5d8e
hendrikjimmy <c4t.why.not@gmail.com>
2016-04-11 16:48:24 +0200

    Encontrado un metodo que se me habia olvidado actualizar y estaba dando error, en concreto getFrequntRenterPoints

--- PISSII/src/refactoringFowler/Customer.java
+++ PISSII/src/refactoringFowler/Customer.java
@@ -20,26 +20,21 @@ public class Customer {
 	};
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		Enumeration rentals = _rentals.elements();
-		String result = "Rental Record for " + getName() + "\n";
-		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
-			Rental each = (Rental) rentals.nextElement();
-
-			thisAmount = each.getCharge();
-
-			frequentRenterPoints += each.getFrequentRenterPoints();
-
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
-		}
-		// add footer lines
-		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
-		return result;
+		 String result = "Rental Record for " + getName() + "\n";
+		 while (rentals.hasMoreElements()) {
+		 Rental each = (Rental) rentals.nextElement();
+		 //show figures for this rental
+		 result += "\t" + each.getMovie().getTitle()+ "\t" +
+		 String.valueOf(each.getCharge()) + "\n";
+		 }
+		 //add footer lines
+		 result += "Amount owed is " +
+		String.valueOf(getTotalCharge()) + "\n";
+		 result += "You earned " +
+		String.valueOf(getTotalFrequentRenterPoints()) +
+		 " frequent renter points";
+		 return result;
 	}
 
 	private double getTotalCharge() {
@@ -54,12 +49,12 @@ public class Customer {
 
 	private int getTotalFrequentRenterPoints() {
 		int result = 0;
-		Enumeration rentals = _rentals.elements();
-		while (rentals.hasMoreElements()) {
-			Rental each = (Rental) rentals.nextElement();
-			result += each.getFrequentRenterPoints();
-		}
-		return result;
+		 Enumeration rentals = _rentals.elements();
+		 while (rentals.hasMoreElements()) {
+		 Rental each = (Rental) rentals.nextElement();
+		 result += each.getFrequentRenterPoints();
+		 }
+		 return result;
 	}
 
 	public String htmlStatement() {
--- PISSII/src/refactoringFowler/Rental.java
+++ PISSII/src/refactoringFowler/Rental.java
@@ -21,8 +21,8 @@ public class Rental {
 		return _movie.getCharge(_daysRented);
 	}
 
-	int getFrequentRenterPoints(int daysRented) {
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+	public int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
 			return 2;
 		else
 			return 1;
